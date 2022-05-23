aee85407e7705161fda482f784be5b429f7006f0
Hendrik Mans <hendrik@mans.de>
2022-05-23 20:17:32 +0200

    Remove turbolinks; make things nicer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), turbolinks()]
+  integrations: [react()],
 });
