bedc58bc6bc75e9da3f993857098b7c33e5ba700
Hendrik Mans <hendrik@mans.de>
2022-05-25 11:54:33 +0200

    Tweak background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,17 +2,17 @@ import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
-const Thingy = () => {
+const Thingy = ({ color, speed = 1, ...props }) => {
   const mesh = useRef();
 
   useFrame((_, dt) => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.15 * dt;
+    mesh.current.rotation.x = mesh.current.rotation.y += speed * dt;
   });
 
   return (
-    <mesh ref={mesh} position={[6, -6, 0]} scale={10}>
+    <mesh ref={mesh} {...props}>
       <dodecahedronBufferGeometry />
-      <meshStandardMaterial />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
@@ -30,10 +30,11 @@ export const Background = () => (
   >
     <Canvas>
       <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
+      <directionalLight position={[0, 0, 5]} />
       <PerspectiveCamera position={[0, 0, 20]} makeDefault />
 
-      <Thingy />
+      <Thingy position={[6, -6, 0]} scale={10} color="#c66" speed={0.05} />
+      <Thingy position={[-14, 14, -50]} scale={30} color="#ccc" speed={-0.02} />
     </Canvas>
   </div>
 );
