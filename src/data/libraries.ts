30ef6a3f2934feebaa475ad31be37f788290e23d
Hendrik Mans <hendrik@mans.de>
2022-06-02 14:12:38 +0200

    Use normal TS for types <3

---
+++ src/data/libraries.ts
@@ -0,0 +1,13 @@
+import { Library } from "../types";
+
+const libraries: Library[] = [
+  {
+    name: "Three.js",
+    url: "https://threejs.org",
+    description:
+      "A fully-featured WebGL and WebGPU game engine written in and for use with TypeScript.",
+    tags: ["renderer", "webgl"],
+  },
+];
+
+export default libraries;
